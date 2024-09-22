98b48e8da2a77a0a6682be35c993c8005125756e
Sultan Sakyp <sultansakyp@ssakyp.lan>
2024-09-22 12:02:43 +0500

    update

--- interaction/output.go
+++ interaction/output.go
@@ -3,6 +3,7 @@ package interaction
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/common-nighthawk/go-figure"
 )
@@ -57,8 +58,15 @@ func DeclareWinner(winner string) {
 }
 
 func WriteLogFile(rounds *[]RoundData) {
-	file, err := os.Create("gamelog.txt")
+	exPath, err := os.Executable()
+	if err != nil {
+		fmt.Println("Writing log file failed. Exiting!")
+		return
+	}
+	exPath = filepath.Dir(exPath)
 
+	file, err := os.Create(exPath + "/gamelog.txt")
+	//file, err := os.Create("gamelog.txt") // => go run .
 	if err != nil {
 		fmt.Println("Saving a log file failed. Exiting.")
 		return
